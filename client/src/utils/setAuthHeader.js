41b94a132014f9cf3cab85494741aa23fa7414e6
gautier <gautier>
2020-06-11 12:34:43 +0200

    nouveau depart

--- client/src/utils/setAuthHeader.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export default function (token) {
-    if (token) {
-        axios.defaults.headers.common['Authorization'] = 'Bearer ' + token
-    } else {
-        axios.defaults.headers.common['Authorization'] = null 
-    }
-}
